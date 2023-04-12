e3302df1a2fc90f4e48d112c979bcf939e37d9f3
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-12 15:00:58 -0300

    enh: integrate prettier for better formatting
    - integate with vite
    - integrate with eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import solid from "solid-start/vite";
+import eslint from "vite-plugin-eslint";
 import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid({ adapter: vercel({}) })],
+  plugins: [
+    solid({ adapter: vercel({}) }),
+    eslint(),
+  ],
 });
