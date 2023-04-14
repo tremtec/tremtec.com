ae428d3ba08623199d73b58aa4937e730d42b9a7
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-13 23:50:21 -0300

    enh: add tests for containers

--- src/components/shared.test.tsx
+++ src/components/shared.test.tsx
@@ -1,6 +1,8 @@
 import { render } from "@solidjs/testing-library";
 import { describe, expect, it } from "vitest";
 import * as shared from "./shared";
+import { StartRouter } from "solid-start/entry-server/StartServer";
+import { Router } from "solid-start/islands/server-router";
 
 describe("NotFound component", () => {
   it("should render with default text", () => {
@@ -14,5 +16,19 @@ describe("NotFound component", () => {
       <shared.NotFound>{expectedText}</shared.NotFound>
     ));
     expect(result.getByText(expectedText)).toBeTruthy();
+  });
+});
+
+describe("Container component", () => {
+  it("should render with default max w size", () => {
+    const expectedText = "not found text";
+    const result = render(() => (
+      <shared.Container>{expectedText}</shared.Container>
+    ));
+    const fragment = result.asFragment();
+
+    expect(result.getByText(expectedText)).toBeTruthy();
+    expect(fragment).toMatch(/max-w-4xl/);
+    expect(fragment).toMatch(/mx-auto/);
   });
 });
