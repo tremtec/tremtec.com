f50c16e3b8d368a13e0e8112b64d973f6b56a9b1
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-13 12:59:22 -0300

    enh: setup initial cloudflare app

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,16 @@
 import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  server: {
+    preset: "cloudflare-pages",
+
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
+  },
+});
